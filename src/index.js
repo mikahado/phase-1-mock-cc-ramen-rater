c638f9f3adbcd4ea4141ce6930bc08e9e8520907
Michael <mikahado@gmail.com>
2022-10-16 16:10:20 -0400

    moves map method, adds  imgs, makes clickable

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-// first, access the ramen db, using fetch 
-// 
-//
 
-
-fetch('db.json')
+fetch('http://localhost:3000/ramens')
 .then((resp) => resp.json())
-.then((data) => bestRamen(data.ramens))
+.then((data) => data.map((ramen) => {  //I put the map iterator HERE instead, to keep all the ramen details linked together
+    showRamen(ramen)                    //then call the function below, which I renamed to showRamen()
+} ))
 
-          
-function bestRamen(ramenData){
-    const best = ramenData.filter((topRamen) => topRamen.rating === 7)
-    console.log(best)
-   }
+function showRamen(ramenData) {
+    //1) create img tag 2) assign image from db to tag 3) then append it to DOM under 'ramen-menu'
+    const menuImg = document.createElement('img')
+    menuImg.src = ramenData.image
+    document.getElementById('ramen-menu').append(menuImg)
 
-// ramenData.filter((topRamen) => topRamen.rating === 10)
+    //makes the menu-photos clickable, then tells them where to go on the DOM
+    menuImg.addEventListener('click', () => {
+
+    //below, you do the same thing for each ramen detail (image, rating, comment): 1) select the html tag, 2) append it to DOM
+
+    const mainImg = document.querySelector('.detail-image') 
+    mainImg.src = ramenData.image;
+
+    const ramenRating = document.getElementById('rating-display')
+    ramenRating.textContent = ramenData.rating
+
+    const ramenComment = document.getElementById('comment-display')
+    ramenComment.textContent = ramenData.comment
+    })
+}
+
