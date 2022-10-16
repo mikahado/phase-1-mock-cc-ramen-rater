c4cb36de58ce88dd05515f9b0d4d373a4b758b59
Michael <mikahado@gmail.com>
2022-10-16 13:30:52 -0400

    'share ramen rater'

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-// write your code here
+// first, access the ramen db, using fetch 
+// 
+//
+
+
+fetch('db.json')
+.then((resp) => resp.json())
+.then((data) => bestRamen(data.ramens))
+
+          
+function bestRamen(ramenData){
+    const best = ramenData.filter((topRamen) => topRamen.rating === 7)
+    console.log(best)
+   }
+
+// ramenData.filter((topRamen) => topRamen.rating === 10)
